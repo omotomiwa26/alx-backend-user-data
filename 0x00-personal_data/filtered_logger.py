e032b45655db494fd774a06edc9bfd9e81a6119b
omotomiwa26 <omotomiwa26@gmail.com>
2024-11-08 00:10:58 +0100

    2. Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
     This module returns the log message obfuscated
 """
@@ -35,6 +36,18 @@ def filter_datum(
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
 
+
+def get_logger() -> logging.Logger:
+    """
+        This Creates a new logger for user data.
+    """
+    logger = logging.getLogger("user_data")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(stream_handler)
+    return logger
 
 class RedactingFormatter(logging.Formatter):
     """
