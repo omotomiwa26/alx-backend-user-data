1e94722bf121fa3c2ae91b0d04b2473bc534f8e8
omotomiwa26 <omotomiwa26@gmail.com>
2024-11-08 02:01:26 +0100

    4 main file

--- 0x00-personal_data/7-main.py
+++ 0x00-personal_data/7-main.py
@@ -2,13 +2,17 @@
 """
 Main file
 """
+try:
 
-get_db = __import__('filtered_logger').get_db
+    get_db = __import__('filtered_logger').get_db
 
-db = get_db()
-cursor = db.cursor()
-cursor.execute("SELECT COUNT(*) FROM users;")
-for row in cursor:
-    print(row[0])
-cursor.close()
-db.close()
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users;")
+    for row in cursor:
+        print(row[0])
+    cursor.close()
+    db.close()
+
+except Exception as e:
+    print(f"Error: {e}")
