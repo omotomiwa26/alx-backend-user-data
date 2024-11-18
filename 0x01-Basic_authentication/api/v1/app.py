83dc5882e50b445a2bd9d7f088e67f4ea45f71ca
omotomiwa26 <omotomiwa26@gmail.com>
2024-11-18 07:08:26 +0100

    5. Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+    The Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+# from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth_type = getenv('AUTH_TYPE', 'auth')
+auth = None
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+else:
+    auth = Auth()
+
+
+@app.before_request
+def before_request() -> str:
+    """
+        Before request huandler
+    """
+    if auth is None:
+        return
+
+    allowed_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if auth.require_auth(request.path, allowed_paths):
+        auth_header = auth.authorization_header(request)
+        user = auth.current_user(request)
+        if auth_header is None:
+            abort(401)
+        if user is None:
+            abort(403)
 
 
 @app.errorhandler(404)
